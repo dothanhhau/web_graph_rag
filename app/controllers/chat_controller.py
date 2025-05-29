7fae7ba8954e9b91912a82f31a05b3606b46dc5b
Hau Do Thanh <haudt@gmail.com>
2025-05-29 21:45:47 +0700

    add stream search and useage query db

--- app/controllers/chat_controller.py
+++ app/controllers/chat_controller.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, request, jsonify
-from app.models.chat_model import get_chat_response
+from flask import Blueprint, render_template, request, jsonify, Response
+from app.models.chat_model import get_chat_response, generate_cypher, query_neo4j, generate_answer
+import time
 
 chat_bp = Blueprint('chat', __name__)
 
@@ -7,8 +8,35 @@ chat_bp = Blueprint('chat', __name__)
 def index():
     return render_template('chat_view.html')
 
+@chat_bp.route('/chatupdate')
+def chatupdate():
+    return render_template('chat_view_update.html')
+
 @chat_bp.route('/chat', methods=['POST'])
 def chat():
     user_msg = request.json.get('message')
     cypher, answer = get_chat_response(user_msg)
     return jsonify({'cypher': cypher, 'answer': answer})
+
+@chat_bp.route('/api/stream', methods=['POST'])
+def chat_stream():
+    user_msg = request.json.get('question')
+
+    def generate():
+        yield f"Đang chuyển câu hỏi tự nhiên thành truy vấn ngôn ngữ cypher...\n"
+        cypher = generate_cypher(user_msg)
+        yield f"{cypher}\n"
+
+        yield f"\nĐang truy vấn cơ sở dữ liệu đồ thị...\n"
+        res_cypher = query_neo4j(cypher)
+        yield f"{res_cypher}\n"
+
+        yield f"\nĐang sinh câu trả lời cuối cùng...\n"
+        final_res = generate_answer(user_msg, res_cypher)
+        yield f"$@%123{final_res}\n"
+
+    return Response(generate(), content_type='text/plain')
+
+@chat_bp.route('/huongdan', methods=['GET'])
+def huongdan():
+    return render_template('huong_dan.html')
--- app/models/chat_model.py
+++ app/models/chat_model.py
@@ -9,3 +9,19 @@ def get_chat_response(query):
         cypher = ''
         answer = 'Câu truy vấn lỗi cú pháp'
     return cypher, answer
+
+def generate_cypher(query):
+    args = {"question": query, "schema": chain.graph_schema}  # Cung cấp schema của đồ thị
+    generated_cypher = chain.cypher_generation_chain.invoke(args)
+    return generated_cypher
+
+def query_neo4j(cypher):
+    if chain.cypher_query_corrector:
+        cypher = chain.cypher_query_corrector(cypher)
+    context = chain.graph.query(cypher)[: chain.top_k]
+    return context
+
+def generate_answer(query, res_cypher):
+    qa_input = {"question": query, "context": res_cypher}
+    final_result = chain.qa_chain.invoke(qa_input)
+    return final_result
